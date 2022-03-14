a5cc1e865f0570f3230dea82f75de46866904eb9
prasad248 <95473027+prasad248@users.noreply.github.com>
2022-03-14 12:01:58 +0530

    Delete singersModel.js

--- singersModel.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize,DataTypes) => {
-    const singers =sequelize.define("singers", {
-      Name : {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-    });
- return singers;
-  };
