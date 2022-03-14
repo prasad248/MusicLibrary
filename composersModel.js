e1581bdc5d344ae6876e77f8341fdd8607c5f664
prasad248 <95473027+prasad248@users.noreply.github.com>
2022-03-14 12:01:03 +0530

    Delete composersModel.js

--- composersModel.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize,DataTypes) => {
-    const Composers =sequelize.define("composers", {
-      Name : {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    });
- return Composers;
-  }; 
